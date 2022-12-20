fb9df1c25344728f5ab23680266ca3c6431a9558
UrbanAss134 <urbanass@bk.ru>
2022-12-20 21:47:21 +0500

    Решена

--- task8.py
+++ task8.py
@@ -8,7 +8,7 @@ my_list = [1, 2, 3, 4, 5, 6]
 print ("Исходнй список : " + str(my_list))
 for i in range(0, len(my_list)-1, 2):
     my_list[i], my_list[i+1] = my_list[i+1], my_list[i]
-    new_my_list = my_list
-    for i in range(0, len(new_my_list)-2, 1):
-        new_my_list[i], new_my_list[i+1] = new_my_list[i+1], new_my_list[i]
+new_my_list = my_list
+for i in range(0, len(new_my_list)-2, 1):
+    new_my_list[i], new_my_list[i+1] = new_my_list[i+1], new_my_list[i]
 print ("Результат смешения : " +  str(new_my_list))
