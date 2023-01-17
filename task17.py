4fa9c714e3fb02b02ee47cb6318f22aa1eb35f22
UrbanAss134 <urbanass@bk.ru>
2023-01-17 21:57:04 +0500

    новое

--- task17.py
+++ task17.py
@@ -3,26 +3,3 @@
 # aaaaabbbcccc -> 5a3b4c
 # 5a3b4c -> aaaaabbbcccc
 # Выполнение алгоритма сжатия данных кодирования длин серий (RLE) для строки `str`
-def encode(s):
-
-    encoding = "" # сохраняет выходную строку
-
-    i = 0
-    while i < len(s):
-        count = 1
-
-    while i + 1 < len(s) and s[i] == s[i + 1]:
-        count = count + 1
-        i += 1
-
-# добавляет к результату текущий символ и его количество
-    encoding += str(count) + s[i]
-    i += 1
-
-    return encoding
-
-
-    if __name__ == '__main__':
-
-        s = 'ABBCCCD'
-print(encode(s))
